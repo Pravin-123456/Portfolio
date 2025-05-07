8efcbab4bdad891464fd9afabd6c468b42d14a03
Pravin <pravin709266@gmail.com>
2025-05-07 21:17:16 +0530

    add myservice hedding and pagecount fixed

--- src/components/PageCount.jsx
+++ src/components/PageCount.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageCount = ({ page }) => {
   return (
-    <div className="absolute bottom-5 right-8 md:bottom-10 md:right-10 text-white text-xl md:text-3xl font-semibold z-50">
+    <div className="absolute bottom-5 right-8 md:bottom-10 md:right-10 text-white text-xl md:text-3xl font-semibold z-50 hidden md:block">
       0{page} / 05
     </div>
   );
