ef5d896ef302489fdf842eeae90ec1a75942544c
Pravin <pravin709266@gmail.com>
2025-05-12 13:21:59 +0530

    optimize the projects component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,47 +1,52 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, memo } from "react";
 import VanillaTilt from "vanilla-tilt";
 import ProjectCard from "../components/ProjectCard";
 import { ProjectInfo } from "../assets/assets";
 import PageCount from "../components/PageCount";
 
-function Tilt({ children }) {
+const tiltOptions = {
+  scale: 1.01,
+  speed: 500,
+  max: 15,
+};
+
+const Tilt = memo(({ children }) => {
   const tiltRef = useRef(null);
 
   useEffect(() => {
-    VanillaTilt.init(tiltRef.current, {
-      scale: 1.01,
-      speed: 500,
-      max: 15,
-    });
+    if (tiltRef.current) {
+      VanillaTilt.init(tiltRef.current, tiltOptions);
+    }
   }, []);
 
   return (
     <div
       ref={tiltRef}
-      className="box h-[300px] w-[250px] bottom-3 relative rounded-xl group backdrop-blur-3xl shadow-2xl backface-hidden antialiased preserve-3d"
+      className="box h-[300px] w-[250px] relative bottom-3 rounded-xl group backdrop-blur-3xl shadow-2xl backface-hidden antialiased preserve-3d"
     >
       {children}
     </div>
   );
-}
+});
 
 const Projects = () => {
   return (
-    <div id="projects"
-      className="relative min-h-screen bg-gradient-to-br from-[#051321] via-[#061d3c] to-[#0f0727]
- text-center py-10"
+    <section
+      id="projects"
+      className="relative min-h-screen py-10 text-center bg-gradient-to-br from-[#051321] via-[#061d3c] to-[#0f0727]"
     >
-      <h2 className="text-3xl text-white mb-15">PROJECTS</h2>
-      <div className="grid md:mx-50 md:grid-cols-2 lg:grid-cols-3 gap-16 justify-items-center text-white">
-        {/* Card 1 */}
+      <h2 className="mb-10 text-3xl text-white">PROJECTS</h2>
+
+      <div className="grid md:mx-50 justify-items-center gap-16 md:grid-cols-2 lg:grid-cols-3 px-6 text-white">
         {ProjectInfo.map((project, index) => (
           <Tilt key={index}>
             <ProjectCard {...project} />
           </Tilt>
         ))}
       </div>
+
       <PageCount page={4} />
-    </div>
+    </section>
   );
 };
 
